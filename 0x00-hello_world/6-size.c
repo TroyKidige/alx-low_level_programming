ea0f9bd991c05e7b18d25e173b498d66eb1fdbcd
TroyKidige <Kidigeisaac@gmail.com>
2023-09-07 16:16:43 -0700

    done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,14 @@
 # include <stdio.h>
 /**
- * main- a script using size
- * Retun: 0 for success
+ * main-a script using size
+ * Return: 0 for success
  */
 int main(void)
 {
 	printf("size of a char: %d  byte(s)", sizeof(char));
-	printf("size of an int:%d  byte(s)",sizeof(int));
-	printf(" Size of a long int: %d byte(s)",sizeof(long int));
-	printf("Size of a long long int: %d byte(s)",sizeof(long long int));
-	printf("Size of a float: %d byte(s)",sizeof(float));
+	printf("size of an int:%d  byte(s)", sizeof(int));
+	printf(" Size of a long int: %d byte(s)", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)", sizeof(long long int));
+	printf("Size of a float: %d byte(s)", sizeof(float));
 	return (0);
 }
