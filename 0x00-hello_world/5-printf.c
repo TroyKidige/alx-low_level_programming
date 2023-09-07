d757947fd603df07de1004de64dbea1bd9ff188b
TroyKidige <Kidigeisaac@gmail.com>
2023-09-07 15:32:59 -0700

    troy

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+/**
+ * main-A script using prinf function
+ * Return:0 for success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\");
+
+	return (0);
+}
