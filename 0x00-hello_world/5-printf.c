cc450c94e55710a48e9d42ab6ca81d97f87b64a1
TroyKidige <Kidigeisaac@gmail.com>
2023-09-07 15:38:12 -0700

    betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 # include <stdio.h>
 /**
- * main-A script using prinf function
- * Return:0 for success
+ * main-a script using printf function
+ * Return: 0 for success
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
