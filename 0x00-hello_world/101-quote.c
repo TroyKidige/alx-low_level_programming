13f84ffe97dd317e8f9038c4d1f4748899854735
TroyKidige <Kidigeisaac@gmail.com>
2023-09-08 02:49:35 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
-int main(void)
-/** 
- * main - C program  without using  printf or puts
- *Return: 1 (for success) 
- */
-{
-write("and that piece of art is useful" \- Dora Korpar, 2015-10-19,life,");
+int main(void){
+
+	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2,quote,59);
 	return (1);
 }
