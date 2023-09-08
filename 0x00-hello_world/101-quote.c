ad313c683393e36af55a677fd6cbed1c571423fa
TroyKidige <Kidigeisaac@gmail.com>
2023-09-08 02:52:26 -0700

    try

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
+/**
+ * main- A program not using printf or puts
+ * Return: 1 (success)
+ */
 int main(void){
 
 	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
