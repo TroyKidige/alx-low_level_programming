bf696eba3aa5c1cbd75c82a3d2f178f1f4faa92f
TroyKidige <Kidigeisaac@gmail.com>
2023-09-08 03:00:23 -0700

    trial

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,8 @@
  * main- A program not using printf or puts
  * Return: 1 (success)
  */
-int main(void){
+int main(void)
+{
 char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 		write(2, quote, 59);
 	return (1);
