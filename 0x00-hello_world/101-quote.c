8717b4e4e05f3c243d3130692273d485597618e5
TroyKidige <Kidigeisaac@gmail.com>
2023-09-08 02:04:25 -0700

    luck

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 int main(void)
 /** 
  * main - function withouth printf or puts
- *Return: 0 (for success) 
+ *Return: 1 (for success) 
  */
 {
-write("and that piece of art is useful" - Dora Korpar, 2015-10-19");
-	return (0);
+write("and that piece of art is useful" \- Dora Korpar, 2015-10-19\n",stdout");
+	return (1);
 }
