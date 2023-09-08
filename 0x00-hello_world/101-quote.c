163889b9a523d5041f31e981578b6c2fc3cbbe89
TroyKidige <Kidigeisaac@gmail.com>
2023-09-08 02:55:20 -0700

    push

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,7 @@
  * Return: 1 (success)
  */
 int main(void){
-
-	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2,quote,59);
+char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, quote, 59);
 	return (1);
 }
