c6f694a0d39c5e12abe1308dcbe24df17254d165
TroyKidige <Kidigeisaac@gmail.com>
2023-09-08 01:48:05 -0700

    test

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+/** 
+ * main - function withouth printf or puts
+ *Return: 0 (for success) 
+ */
+{
+char("and that piece of art is useful" - Dora Korpar, 2015-10-19");
+	return (0);
+}
