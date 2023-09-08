9135d821ae311372cc4d3eaf2e41cf364647628e
TroyKidige <Kidigeisaac@gmail.com>
2023-09-08 02:14:04 -0700

    push

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 int main(void)
 /** 
- * main - function withouth printf or puts
+ * main - C program  without using  printf or puts
  *Return: 1 (for success) 
  */
 {
-write("and that piece of art is useful" \- Dora Korpar, 2015-10-19\n",stdout");
+write("and that piece of art is useful" \- Dora Korpar, 2015-10-19,life,");
 	return (1);
 }
