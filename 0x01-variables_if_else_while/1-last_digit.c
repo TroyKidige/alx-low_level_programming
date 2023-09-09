45246c92483070553854e3e477508eed6783ba45
TroyKidige <Kidigeisaac@gmail.com>
2023-09-09 14:58:56 -0700

    last

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,10 +13,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 5)
-		printf("Last digit of %d and is greater than %d\n", n, n);
+		printf("Last digit of %d and is greater than 5\n", n, n);
 	if (n == 0)
-		printf("Last digit of %d and is %d\n", n, n);
+		printf("Last digit of %d and is 0\n", n, n);
 	if (n < 6 && (n % 10) != 0)
-		printf("Last digit of %d and is less than %d  and not %d\n", n, n, n % 10);
+		printf("Last digit of %d and is less than 6  and not 0\n", n, n, n % 10);
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - alphabets
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+	putchar('\n');
+	}
+	return 0;
+}
