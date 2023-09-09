83da2f5b89461b467a764b259af69869f2910ef5
TroyKidige <Kidigeisaac@gmail.com>
2023-09-09 16:32:03 -0700

    main

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point of all program
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int cha;
+
+	cha = 'a'
+	while (cha <= 'z')
+}
+	if (cha != 'e' || cha != 'q')
+	}
+	putchar(cha);
+	}
+	chat++
