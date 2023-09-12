7cbf18174f04d011d0ab9c125bce10d7bf4cd3b5
TroyKidige <Kidigeisaac@gmail.com>
2023-09-12 13:31:41 -0700

    push

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - A program that prints the alphabets 10 times
+ * print_alphabet_x10 -  A program that prints the alphabets 10 times
  *Return: 0 (success)
  */
 void print_alphabet_x10(void)
@@ -8,6 +8,7 @@ void print_alphabet_x10(void)
 	char ch;
 
 	int a;
+
 	a = 0;
 
 while (a < 10)
