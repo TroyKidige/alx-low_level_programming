c5cd5171bfc7050e2a3ec7295f34b48df6700b46
TroyKidige <Kidigeisaac@gmail.com>
2023-09-13 21:07:12 +0000

    phone

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_last_digit - A program that prints last digts
+ *@n: the number to be treated
+ *Return: 0 (success)
+ */
+int print_last_digit(int)
+{
+	int last;
+
+	last = % 10;
+	if (last < 0)
+	{
+
+	last = last * -l;
+}
+	_putchar(last * '0');
+	return (0);
+}	
