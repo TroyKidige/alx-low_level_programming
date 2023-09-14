84d80723c33ed2d47cd55b1e37ac18b5ed85cf2f
TroyKidige <Kidigeisaac@gmail.com>
2023-09-14 15:07:53 -0700

    push

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -19,4 +19,6 @@ void print_to_98(int n);
 int print_sign(int n);
 int main(void);
 int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 #endif /* MAIN_H */
