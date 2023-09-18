41dd9c6acba67485193c8e6524a49abccc4e74bf
TroyKidige <Kidigeisaac@gmail.com>
2023-09-18 09:05:11 -0700

    fear

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - a function that prints a string, followed by a new line
+ * @str: the string
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	{
+		_putchar('\n');
+	}
+}
+
