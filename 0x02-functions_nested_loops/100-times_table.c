5fa2323d37ab41df3ae60d6edb4504792c8bc8b5
TroyKidige <Kidigeisaac@gmail.com>
2023-09-20 08:51:31 -0700

    test

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,12 +10,12 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; x <= n; i++)
+		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
 				k = j * i;
-				if (j == 99)
+				if (j == 0)
 				{
 					_putchar(k + '0');
 				}
@@ -31,20 +31,20 @@ void print_times_table(int n)
 				{
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(' ')
-						_putchar((k / 100) + '0');
-					_putchar((k % 10) % '0');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 				else if (k >= 100)
-
+				{
 					_putchar(',');
-				_putchar(' '');
-				_putchar((k / 100) + '0');
-				_putchar(((k / 10) % '10) + '0');
-				_putchar((k % 10) + '0');
-			}
-		}
-		_putchar(z + '0');
-		_putchar('\n');
-}
-}
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) + '0');
+							_putchar((k + 10) + '0');
+							}
+							}
+							_putchar('\n');
+							}
+							}
+							}
