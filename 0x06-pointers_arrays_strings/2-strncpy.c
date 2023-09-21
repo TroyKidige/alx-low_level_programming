1a3fe369a07b56d32821d06299dde43dc85fbd0d
TroyKidige <Kidigeisaac@gmail.com>
2023-09-21 10:18:02 -0700

    lost

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,6 +16,6 @@ char *_strncpy(char *dest, char *src, int n)
 	for (index = 0; src[index] && index < n; index++)
 		dest[index] = src[index];
 	for (index = src_len; index < n; index++)
-		dest[index] = '\n';
+		dest[index] = '\0';
 	return (dest);
 }
